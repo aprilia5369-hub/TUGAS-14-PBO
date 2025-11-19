f802596ad74540bb6f4c3d8cde6066c49c5cee4b
aprilia5369-hub <aprilia5369@gmail.com>
2025-11-19 13:12:14 +0700

    APRILIA

---
+++ TUGAS-14/build/generated-sources/ap-source-output/Bank/Pw_.java
@@ -0,0 +1,14 @@
+package Bank;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-11-11T17:20:32", comments="EclipseLink-2.7.12.v20230209-rNA")
+@StaticMetamodel(Pw.class)
+public class Pw_ { 
+
+    public static volatile SingularAttribute<Pw, String> password;
+    public static volatile SingularAttribute<Pw, String> username;
+
+}
